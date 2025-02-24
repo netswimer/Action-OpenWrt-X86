1f652301027f15c5f72b6f64e847e05d702613e1
netswimer <gyf319@gmail.com>
2025-02-24 20:06:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,4 +18,4 @@ sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_genera
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
- sed -i 's/OpenWrt/MyAngelRouter/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/MyAngelRouter/g' package/base-files/files/bin/config_generate
